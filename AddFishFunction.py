be9c48aa0ddc6d1a93f154b4d93d9f05c5410e16
Monti <ooy.yoo@gmail.com>
2023-06-03 14:58:50 +0800

    v1

---
+++ AddFishFunction.py
@@ -0,0 +1,24 @@
+import requests
+import subprocess
+
+# 下載function.text檔案 #
+url = 'https://github.com/YC815/MyFishFunction/raw/main/function.text'
+filename = 'function.text'
+
+response = requests.get(url)
+response.raise_for_status()
+
+with open(filename, 'wb') as file:
+    file.write(response.content)
+
+# 寫入內容 #
+function_file = 'function.text'
+config_file = '.dotfiles/fish/.config/fish/config.fish'
+
+with open(function_file, 'r') as f:
+    function_text = f.read()
+
+with open(config_file, 'a') as f:
+    f.write('\n' + function_text)
+
+subprocess.call(['sh', 'ok.sh'])
---
+++ ins.sh
@@ -0,0 +1,2 @@
+pip install requests
+python3 AddFishFunction.py
