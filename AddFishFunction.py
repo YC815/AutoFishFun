9da22d30f8ccadea27e81f21ff63a51ad08b6fd5
YC815 <minnecraft.yc815@gmail.com>
2023-06-03 17:07:24 +0800

    current_path

--- AddFishFunction.py
+++ AddFishFunction.py
@@ -20,7 +20,10 @@ with open(function_file, 'r') as f:
     function_text = f.read()
 username = os.getlogin()
 os.chdir(f'/Users/{username}')
+os.chdir(f'/Users/{username}')
 with open(config_file, 'a') as f:
     f.write('\n' + function_text)
 
+current_path = os.getcwd()
+os.chdir(current_path)
 subprocess.call(['sh', 'ok.sh'])
