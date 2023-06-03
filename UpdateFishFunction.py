0fddedcae7d454c0da5a8e39af4253f7d88e631a
YC815 <minnecraft.YC815@gmail.com>
2023-06-03 19:59:44 +0800

    ccccc

--- UpdateFishFunction.py
+++ UpdateFishFunction.py
@@ -28,7 +28,7 @@ with open(config_file, 'r+') as f:
     f.seek(0)
     function_start = None
     for i, line in enumerate(lines):
-        if line.strip() == '# yushun\'s funstion':
+        if line.strip() == '# yushun\'s function':
             function_start = f.tell()
             break
 
