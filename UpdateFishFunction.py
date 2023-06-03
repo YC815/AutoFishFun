91f97b294925651ad3b60be78fd6db398a2f6986
YC815 <minnecraft.YC815@gmail.com>
2023-06-03 19:51:10 +0800

    upd

--- UpdateFishFunction.py
+++ UpdateFishFunction.py
@@ -1,9 +1,11 @@
 import requests
 import subprocess
 import os
+
 script_path = os.path.abspath(__file__)
 script_dir = script_path.rsplit("/", 1)[0]
-# 下載function.text檔案 #
+
+# 下载function.text文件 #
 url = 'https://github.com/YC815/MyFishFunction/raw/main/function.text'
 filename = 'function.text'
 
@@ -13,7 +15,7 @@ response.raise_for_status()
 with open(filename, 'wb') as file:
     file.write(response.content)
 
-# 更新function檔 #
+# 更新function文件 #
 function_file = 'function.text'
 username = os.getlogin()
 config_file = f'/Users/{username}/.dotfiles/fish/.config/fish/config.fish'
@@ -21,55 +23,23 @@ config_file = f'/Users/{username}/.dotfiles/fish/.config/fish/config.fish'
 with open(function_file, 'r') as f:
     function_text = f.read()
 
-with open(config_file, 'r+') as f:
+with open(config_file, 'r') as f:
     lines = f.readlines()
-    f.seek(0)
-    index = 0
-    for i, line in enumerate(lines):
-        if line.strip() == '# yushun\'s funstion':
-            index = i
-            break
-    for line in lines[:index]:
-        f.write(line)
-    f.write(function_text)
-    f.truncate()
+
+index = None
+for i, line in enumerate(lines):
+    if line.strip() == '# yushun\'s funstion':
+        index = i
+        break
+
+if index is not None:
+    lines = lines[:index]  # 删除 # yushun's funstion 及之后的代码
+
+lines.append('\n')  # 添加换行
+lines.append(function_text)  # 添加函数文本
+
+with open(config_file, 'w') as f:
+    f.writelines(lines)
+
 username = os.getlogin()
 subprocess.call(['sh', f'{script_dir}/ok.sh'])
-
-# import requests
-# import subprocess
-# import os
-# script_path = os.path.abspath(__file__)
-# script_dir = script_path.rsplit("/", 1)[0]
-
-# # 下載function.text檔案 #
-# url = 'https://github.com/YC815/MyFishFunction/raw/main/function.text'
-# filename = 'function.text'
-
-# response = requests.get(url)
-# response.raise_for_status()
-
-# with open(filename, 'wb') as file:
-#     file.write(response.content)
-
-# # 更新function檔 #
-# function_file = 'function.text'
-# username = os.getlogin()
-# config_file = f'/Users/{username}/.dotfiles/fish/.config/fish/config.fish'
-
-# with open(function_file, 'r') as f:
-#     function_text = f.read()
-
-# with open(config_file, 'a') as f:
-#     lines = f.readlines()
-#     f.seek(0)
-#     index = 0
-#     for i, line in enumerate(lines):
-#         if line.strip() == '# yushun\'s funstion':
-#             index = i
-#             break
-#     for line in lines[:index]:
-#         f.write(line)
-#     f.write(function_text)
-#     f.truncate()
-
